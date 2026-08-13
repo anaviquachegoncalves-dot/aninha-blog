da54988ee60f14ee25894accc3b49845f069aa87
anaviquachegoncalves-dot <ana.viquache.goncalves@escola.pr.gov.br>
2026-08-13 09:53:33 -0300

    blog aninha

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const botoes = decument.queryselectorAll('button');
 
 botoes.forEach(botao => { 
      botao.addEventlistener('click', curtir)
-
+      let curtiu = false
 function curtir() {
      let curtidas = document.queryselector('span')
-     curtidas.textcontent++
+    if(curtiu == false){
+        curtiu = true
+        curtidas.textcontent++
+        botao.style.beckgroundColor = 'blue'
+    }
     }
 });

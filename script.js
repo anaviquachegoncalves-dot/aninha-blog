632115f33adfb1dfc744e8bef2871d938c64fefe
anaviquachegoncalves-dot <ana.viquache.goncalves@escola.pr.gov.br>
2026-08-18 09:51:32 -0300

    blog

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ botoes.forEach(botao => {
      botao.addEventlistener('click', curtir)
       let curtiu = false
 function curtir() {
-     let curtidas = document.queryselector('span')
+     let curtidas = botao.queryselector('span')
     if(curtiu == false){
         curtiu = true
         curtidas.textcontent++
         botao.style.beckgroundColor = 'blue'
+    }else{
+        curtiu = false
+        curtidas.textcontent--
+        botao.style.beckgroundColor =lightgr
     }
     }
 });
